c270081646ac50b13999e7b22e40768fdd0b60aa
Mayuran <mayuranswee23@gmail.com>
2021-05-24 13:47:56 -0400

    added editting ability

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
 $("#currentDay").text(moment().format("MMMM Do YYYY"));
 
 //current time
-var currentTime = moment().format('LT');
+// var currentTime = moment().format ("LT");
+var currentTime = moment()
+// var roundedTime = currentTime.startOf ("hour");
 
 //obtain time rows
 var hourNine = document.getElementById("nine")
@@ -18,7 +20,7 @@ var hourFive = document.getElementById("five");
 
 
 // get time blocks per hour
-var timeNine = moment().hour(9);
+var timeNine = moment().hour(9)
 var timeTen = moment().hour(10);
 var timeEleven = moment().hour(11);
 var timeTwelve = moment().hour(12);
@@ -29,13 +31,36 @@ var timeFour = moment().hour(16);
 var timeFive = moment().hour(17);
 
 var time = [timeNine, timeTen, timeEleven, timeTwelve, timeOne, timeTwo, timeThree, timeFour, timeFive]
+console.log(currentTime)
+console.log(timeNine)
+// time at 9 am
+
+function checkTime () {
+    if (currentTime.isAfter(timeNine)){
+        console.log("hi")
+    }
+    
+}
+
+// click on paragraph text to enter input
+$(".taskText").on("click","p", function(){
+    var text = $(this).text().trim();
+    var textInput = $("<textarea>").addClass("textarea").val(text);
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});
 
 
+//save to localstorage
 
+var saveButton = document.querySelector(".btn");
+saveButton.addEventListener("click", function(){
+    console.log("hi")
+})
 
-
-
-
+// $(".btn").on("click", "button", function(){
+//     console.log("hi");
+// })
 
 
 
